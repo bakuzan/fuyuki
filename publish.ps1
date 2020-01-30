fb3b97eb87921b24b30c74d5f736fbc372ff23f9
bakuzan <imannoyed4days@hotmail.co.uk>
2020-01-30 13:39:16 +0000

    Improved publish script.

--- publish.ps1
+++ publish.ps1
@@ -1,5 +1,40 @@
+function Get-ScriptDirectory { Split-Path $MyInvocation.ScriptName }
+function Get-OuputColour($v) { if ($v) { return "Blue" } else { return "Green" } }
+
+
+Write-Host "";
+$user_input = Read-Host -Prompt "Do you want to release Fuyuki after publishing?"
+Write-Host "";
+
+$release = $false;
+$src = Join-Path (Get-ScriptDirectory) "\bin\Release\netcoreapp3.1\publish\*";
+$dst = "C:\inetpub\wwwroot\fuyuki";
+
+if (@("true", "false", "yes", "no", "y", "n") -contains $user_input) {
+    $release = @("true", "yes", "y") -contains $user_input
+}
+else {
+    Write-Host "Invalid release input. Enter yes or no. (y or n)" -ForegroundColor "Red";
+    Read-Host -Prompt "Press Enter to exit";
+    exit
+}
+
+# Start actual work...
+
 dotnet publish --configuration Release;
 
-Write-Host "Fuyuki publish complete.";
+Write-Host "";
+Write-Host "Fuyuki published." -ForegroundColor (Get-OuputColour $release);
+Write-Host "";
+
+if ($release) {
+    Write-Host "Copying release to target" -ForegroundColor "Blue";
+
+    Copy-Item -Path $src -Destination $dst -Exclude "*.db" -Recurse -Container -Force
+    
+    Write-Host "";
+    Write-Host "Fuyuki released." -ForegroundColor "Green";
+    Write-Host "";
+}
 
 Read-Host -Prompt "Press Enter to exit";
