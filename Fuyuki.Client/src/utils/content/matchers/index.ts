c137951a22f293d7145a78eb926b790e404ec5e2
bakuzan <imannoyed4days@hotmail.co.uk>
2020-02-18 13:29:00 +0000

    Move matcher template.

--- Fuyuki.Client/src/utils/content/matchers/index.ts
+++ Fuyuki.Client/src/utils/content/matchers/index.ts
@@ -7,32 +7,3 @@ export { default as pornhub } from './pornhub';
 export { default as streamable } from './streamable';
 export { default as vreddit } from './vreddit';
 export { default as wikipedia } from './wikipedia';
-
-/* Matcher template
-*
-
-// tslint:disable:object-literal-sort-keys 
-import { Post } from '../../../interfaces/Post';
-import { ContentMatcher } from '../types/ContentMatcher';
-import { ContentMeta } from '../types/ContentMeta';
-import { ContentType } from '../types/ContentType';
-
-function match(post: Post) {
-  return false;
-}
-
-async function meta(post: Post): Promise<ContentMeta> {
-  return {
-    type: ContentType.isNone
-  };
-}
-
-const matcher: ContentMatcher = {
-  match,
-  meta
-};
-
-export default matcher;
-
-*
-*/
