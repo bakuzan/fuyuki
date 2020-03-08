05c98572bc19514dff1002a41f0cb2d3c3ce69af
bakuzan <imannoyed4days@hotmail.co.uk>
2020-03-08 07:45:22 +0000

    WIP html replacements.

--- Fuyuki.Client/src/utils/htmlBodyReplacements.ts
+++ Fuyuki.Client/src/utils/htmlBodyReplacements.ts
@@ -3,5 +3,36 @@ export default function htmlBodyReplacements(html: string) {
     return html;
   }
 
-  return html.replace('href="/u/', 'href="https://www.reddit.com/u/');
+  const codeBlocks = html.match(/```([^```]*)```/g);
+  if (codeBlocks) {
+    codeBlocks.forEach((m) => {
+      html.replace(m, `<code>${m.replace(/`/g, '')}</code>`);
+    });
+  }
+
+  const parser = new DOMParser();
+  const htmlDoc = parser.parseFromString(html, 'text/html');
+
+  const anchors = Array.from(htmlDoc.getElementsByTagName('a'));
+  anchors.forEach((an) => {
+    const href = an.getAttribute('href') ?? '';
+
+    if (href.startsWith('/u/')) {
+      html = html.replace(href, `https://www.reddit.com${href}`);
+    } else if (href.startsWith('https://www.reddit.com/r/')) {
+      const [subname, comment, postId] = href
+        .replace('https://www.reddit.com/r/', '')
+        .split('/');
+
+      const isPost = comment === 'comments';
+
+      const preferredUrl = isPost
+        ? `/r/${subname}/t3_${postId}/comments`
+        : `/r/${subname}`;
+
+      html = html.replace(href, preferredUrl);
+    }
+  });
+
+  return html;
 }
