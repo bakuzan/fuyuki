010dd29668ce5c33be0ab134ac4e71c30e588b84
bakuzan <imannoyed4days@hotmail.co.uk>
2020-11-05 09:36:34 +0000

    Improve video detection.

--- Fuyuki.Client/src/utils/content/matchers/baseVideo.ts
+++ Fuyuki.Client/src/utils/content/matchers/baseVideo.ts
@@ -1,10 +1,12 @@
 /* tslint:disable:object-literal-sort-keys */
+import videoExtensions from 'video-extensions';
+
 import { Post } from '../../../interfaces/Post';
 import { ContentMatcher } from '../types/ContentMatcher';
 import { ContentMeta } from '../types/ContentMeta';
 import { ContentType } from '../types/ContentType';
 
-function match(post: Post) {
+function isDomainMatch(post: Post) {
   const embedDomains = [
     'twitch.tv',
     'youtube.com/watch',
@@ -15,12 +17,32 @@ function match(post: Post) {
   return (post.url && embedDomains.some((x) => post.url.includes(x))) || false;
 }
 
+function isExtensionMatch(post: Post) {
+  const vidExts = new Set(videoExtensions);
+  const ext = post.url.split('.').pop() ?? '';
+  return vidExts.has(ext);
+}
+
+function match(post: Post) {
+  return isDomainMatch(post) || isExtensionMatch(post);
+}
+
 async function meta(post: Post): Promise<ContentMeta> {
-  return {
-    type: ContentType.isIframe,
-    src: `//www.redditmedia.com/mediaembed/${post.id}`,
-    defaultHeight: 350
-  };
+  if (isDomainMatch(post)) {
+    return {
+      type: ContentType.isIframe,
+      src: `//www.redditmedia.com/mediaembed/${post.id}`,
+      defaultHeight: 350
+    };
+  } else {
+    // Should never be empty string, but typescript complains.
+    const ext = post.url.split('.').pop() ?? '';
+
+    return {
+      type: ContentType.isVideo,
+      sources: [{ src: post.url, type: `video/${ext}` }]
+    };
+  }
 }
 
 const matcher: ContentMatcher = {
